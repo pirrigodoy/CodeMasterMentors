36e78cb2ceb58f3958d42c9b9bdec7500ca90005
Joel Comes Rodriguez <joelcomes13@gmail.com>
2024-04-10 14:20:50 +0200

    10/04/2024
    Arreglado los problemas con la home

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './services/auth.service'; // Asegúrate de importar el servicio AuthService
-
+import { ApiService } from './services/api.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,11 +9,14 @@ import { AuthService } from './services/auth.service'; // Asegúrate de importar
 export class AppComponent implements OnInit {
   isLoggedIn: boolean = false; // Inicializa isLoggedIn como falso por defecto
   showDropdown: boolean = false;
+  users: any = [];
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private apiService:ApiService
+  ) { }
 
   ngOnInit() {
     // Suscribe a isLoggedIn$ para actualizar isLoggedIn cuando cambie el estado de autenticación
+    this.getUsers();
     this.authService.isLoggedIn$.subscribe(isLoggedIn => {
       this.isLoggedIn = isLoggedIn;
     });
@@ -26,6 +29,13 @@ export class AppComponent implements OnInit {
 
   toggleDropdown() {
     this.showDropdown = !this.showDropdown;
+  }
+
+  getUsers() {
+    return this.apiService.getUsers().subscribe((users: {}) => {
+      this.users = users;
+      console.log('Users:', users);
+    })
   }
 }
 
