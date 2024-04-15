921bd9e0f8fec10a2b2f24b4e4def3bfd69759d3
Joel Comes Rodriguez <joelcomes13@gmail.com>
2024-04-15 13:47:21 +0200

    15/04/2024
    Hemos añadido el select con los roles en el registro, tenemos el home filtrado, tenemos un buscador pero no funcional.

--- Backend/app/Http/Controllers/RoleController.php
+++ Backend/app/Http/Controllers/RoleController.php
@@ -3,8 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Role;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use App\Http\Responses\ApiResponse;
 
 class RoleController extends Controller
 {
-    //
+    public function index()
+    {
+        try {
+            $roles = Role::all(['name']); // Obtener solo el campo 'name'
+            return ApiResponse::success('Lista de roles', 200, $roles);
+        } catch (\Exception $e) {
+            return ApiResponse::error('Error al obtener la lista de roles', 500);
+        }
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            $request->validate([
+                'name' => 'required',
+                // Puedes agregar más reglas de validación según tus necesidades
+            ]);
+            $role = Role::create($request->only('name')); // Crear el rol con solo el campo 'name'
+            return ApiResponse::success('Rol creado exitosamente', 201, $role);
+        } catch (ValidationException $e) {
+            $errors = $e->validator->errors()->toArray();
+            return ApiResponse::error('Error de validación', 422, $errors);
+        }
+    }
+
+    // Métodos show(), update() y destroy() similares al ProgrammingLanguageController
+    // Pero trabajando solo con el campo 'name' en lugar de todo el modelo Role
 }
--- Backend/database/factories/UserFactory.php
+++ Backend/database/factories/UserFactory.php
@@ -22,7 +22,7 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
-        $roles = [1, 2, 3]; // IDs de los roles disponibles
+        $roles = [1, 2]; // IDs de los roles disponibles
 
         return [
             'username' => $this->faker->userName,
