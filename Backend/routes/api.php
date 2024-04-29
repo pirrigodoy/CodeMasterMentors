21bd246258f3b0c815703981631dfc7428d920b0
Joel Comes Rodriguez <joelcomes13@gmail.com>
2024-04-29 11:03:57 +0200

    Update api.php

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProgrammingLanguageController;
 use App\Http\Controllers\AdvertisementController;
+use App\Http\Controllers\CommentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -34,4 +35,4 @@ Route::apiResource('roles', RoleController::class);
 Route::apiResource('users', UserController::class);
 Route::apiResource('advertisements', AdvertisementController::class);
 Route::apiResource('programminglanguages', ProgrammingLanguageController::class);
-
+Route::apiResource('comments', CommentController::class);
