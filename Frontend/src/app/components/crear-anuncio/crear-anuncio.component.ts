748358e274a5f82ffe9b6d26978d0c7b84552756
Joel Comes Rodriguez <joelcomes13@gmail.com>
2024-05-03 14:28:27 +0200

    aaa

--- Frontend/src/app/components/crear-anuncio/crear-anuncio.component.ts
+++ Frontend/src/app/components/crear-anuncio/crear-anuncio.component.ts
@@ -1,17 +1,18 @@
 // mis-anuncios.component.ts
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ApiService } from 'src/app/services/api.service';
 @Component({
   selector: 'app-mis-anuncios',
   templateUrl: './crear-anuncio.component.html',
   styleUrls: ['./crear-anuncio.component.css']
 })
-export class CrearAnuncioComponent implements OnInit{
+export class CrearAnuncioComponent implements OnInit {
   nuevoAnuncio: any = {
     user_id: localStorage.getItem('user_id') // Obtener user_id del local_storage
   };  // Objeto para almacenar los datos del nuevo anuncio
   programmingLanguages: any = [];
-  constructor(private apiService: ApiService) { }
+  constructor(private apiService: ApiService, private router: Router) { }
 
   ngOnInit(): void {
 
@@ -20,13 +21,18 @@ export class CrearAnuncioComponent implements OnInit{
   crearAnuncio(): void {
     this.apiService.crearAnuncio(this.nuevoAnuncio).subscribe((response: any) => {
       console.log('Anuncio creado:', response);
-      // Aquí podrías realizar alguna acción adicional, como mostrar un mensaje de éxito o redirigir al usuario
+      // Obtener el advertisement_id del localStorage
+      const advertisementId = localStorage.getItem('advertisement_id');
+      // Redirigir al usuario a la página de "mis-anuncios" con el ID del anuncio como parámetro
+      this.router.navigate(['/mis-anuncios', advertisementId]);
     });
   }
+
+
   getProgrammingLanguages() {
     this.apiService.getProgrammingLanguages().subscribe((response: any) => {
-        this.programmingLanguages = response.data;
-        console.log('ProgrammingLanguages:', this.programmingLanguages);
+      this.programmingLanguages = response.data;
+      console.log('ProgrammingLanguages:', this.programmingLanguages);
     });
   }
 
